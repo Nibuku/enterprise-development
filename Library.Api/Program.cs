ce074707af415de67efeb51c6a8eb88734938c7d
Вероника Будюк <veronika.budyuk2004@gmail.com>
2025-10-19 12:54:37 +0400

    Add XML comments to see summary in swagger.

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -30,6 +30,13 @@ builder.Services.AddScoped<BookCheckoutService>();
 builder.Services.AddScoped<PublicationTypeService>(); 
 builder.Services.AddScoped<LibraryAnalyticsService>();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
